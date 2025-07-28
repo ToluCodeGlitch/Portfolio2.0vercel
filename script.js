901989ff3253210fda213797d954aedf9b653422
ToluCodeGlitch <africanhorrorstory2@gmail.com>
2025-07-28 19:30:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('Particles loaded.');
+// Load particle background
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('Particles.js config loaded.');
 });
